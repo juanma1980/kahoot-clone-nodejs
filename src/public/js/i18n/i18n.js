9082691527a900a96803cbf4f4bc77ca1d338280
juanma1980 <juanma1980@gmail.com>
2020-05-14 10:38:28 +0200

    Basic i18n library

--- src/public/js/i18n/i18n.js
+++ src/public/js/i18n/i18n.js
@@ -1,161 +1,262 @@
 function get_i18n(pagename)
 {
 	i18n={
-		"index":
-			{
-			"en":
-				{"pagename": "Join | LlixHoot",
-					"title": "Join a Game",
-					"namelabel": "Display Name",
-					"pinlabel": "Game Pin",
-					"joinButton": "Join",
-					"host": "Click here to host a LlixHoot!"
-				},
-			"es":
-				{ "pagename": "Unirse | LlixHoot",
-					"title": "Unirse a una partida",
-					"namelabel": "Alias",
-					"pinlabel": "Pin del juego",
-					"joinButton": "Jugar",
-					"host": "¡Pulsa aquí para crear un LlixHoot!"
-				},
-			"ca":
-				{"pagename": "Join | LlixHoot",
-					"title": "Join a Game",
-					"namelabel": "Display Name",
-					"pinlabel": "Game Pin",
-					"joinButton": "Join",
-					"host": "Click here to host a LlixHoot!"
-				}
-			},
-		"quiz":
-			{
-			"en":
-				{"pagename": "Create | LlixHoot",
-					"title": "Start a Kahoot Game",
-					"back": "Back",
-					"subtitle": "Choose a Game Below or",
-					"subtitle_link": "Create your Own!"
-				},
-			"es":
-				{"pagename": "Crear | LlixHoot",
-					"title": "Crea un LlixHoot",
-					"back": "Volver",
-					"subtitle": "Selecciona uno de la lista o",
-					"subtitle_link": "Crea el tuyo propio!"
-				},
-			"ca":
-				{"pagename": "Create | LlixHoot",
-					"title": "Start a Kahoot Game",
-					"back": "Back",
-					"subtitle": "Choose a Game Below or",
-					"subtitle_link": "Create your Own!"
-				}
-			},
-		"quiz_new":
-			{
-			"en":
-				{"pagename":"Creator | KahootClone",
-					"title":"Llixhoot Quiz Creator Studio",
-					"quizTitle": "Quiz Title",
-					"q1": "Question 1: ",
-					"a1":"Answer 1: ",
-					"a2":"Answer 2: ",
-					"a3":"Answer 3: ",
-					"a4":"Answer 4: ",
-					"correct1": "Correct Answer (1-4) :",
-					"another": "Add another question",
-					"create": "Create Quiz",
-					"cancel": "Cancel quiz and return to quiz selection"
-				},
-			"es":
-				{"pagename": "Creator | LlixHoot",
-					"title": "Nombre del cuestionario",
-					"quizTitle": "Nombre del cuestionario",
-					"q1": "Pregunta 1: ",
-					"a1":"Respuesta 1: ",
-					"a2":"Respuesta 2: ",
-					"a3":"Respuesta 3: ",
-					"a4":"Respuesta 4: ",
-					"correct1": "Respuesta correcta (1-4) :",	
-					"another": "Añadir otra",
-					"create": "Crear",
-					"cancel": "Cancelar"
-				},
-			"ca":
-				{"pagename":"Creator | KahootClone",
-					"title":"Llixhoot Quiz Creator Studio",
-					"quizTitle": "Quiz Title",
-					"q1": "Question 1: ",
-					"a1":"Answer 1: ",
-					"a2":"Answer 2: ",
-					"a3":"Answer 3: ",
-					"a4":"Answer 4: ",
-					"correct1": "Correct Answer (1-4) :",
-					"another": "Add another question",
-					"create": "Create Quiz",
-					"cancel": "Cancel quiz and return to quiz selection"
-				}
-			},
-		"host":
+		"en":
 		{
-			"en":
-				{"pagename": "Host | LlixHoot",
-					"title": "Join this Game using the Game Pin: ",
-					"cancel": "Cancel Game",
-					"start": "Start Game"
-				},
-			"es":
-				{"pagename": "Host | LlixHoot",
-					"title": "&Uacute;nete introduciendo el pin: ",
-					"cancel": "Cancelar",
-					"start": "Iniciar"
-				},
-			"ca":
-				{"pagename": "Host | LlixHoot",
-					"title": "Join this Game using the Game Pin: ",
-					"cancel": "Cancel Game",
-					"start": "Start Game"
-				}
+			"index":
+			{
+				"Join | LlixHoot":"Join | LlixHoot",
+				"Join a Game": "Join a Game ",
+				"Display Name": "Display Name",
+				"Game Pin": "Game Pin",
+				"Join": "Join",
+				"Click here to host a kahoot!": "Click here to host a LlixHoot!"
+			},
+			"quiz":
+			{
+				"Create | LlixHoot": "Create | LlixHoot",
+				"Start a Kahoot Game": "Start a Kahoot Game",
+				"Back": "Back",
+				"Choose a Game Below or ": "Choose a Game Below or",
+				"Create your Own!": "Create your Own!"
+			},
+			"quiz_new":
+			{
+				"Creator | KahootClone":"Creator | KahootClone",
+				"Kahoot Quiz Creator Studio":"Llixhoot Quiz Creator Studio",
+				"Quiz Title": "Quiz Title",
+				"Question 1: ": "Question 1: ",
+				"Answer 1: ":"Answer 1: ",
+				"Answer 2: ":"Answer 2: ",
+				"Answer 3: ":"Answer 3: ",
+				"Answer 4: ":"Answer 4: ",
+				"Correct Answer (1-4) :": "Correct Answer (1-4) :",
+				"Add another question": "Add another question",
+				"Create Quiz": "Create Quiz",
+				"Cancel quiz and return to quiz selection": "Cancel quiz and return to quiz selection"
+			},
+			"host":
+			{
+				"Host | LlixHoot": "Host | LlixHoot",
+				"Join this Game using the Game Pin: ": "Join this Game using the Game Pin: ",
+				"Cancel Game": "Cancel Game",
+				"Start Game": "Start Game"
+			},
+			"game":
+			{
+				"Game | LlixHoot": "Game | LlixHoot",
+				"Question 1 / x": "Question 1 / x",
+				"Player's answer: 0 / x":"Player's answer: 0 / x",
+				"Time left:": "Time left:",
+				"Question": "Question",
+				"Answer": "Answer",
+				"Next Question":"Next Question"
+			},
+			"player":
+			{
+				"Lobby | KahootClone": "Lobby | LlixHoot",
+				"Waiting on host to start game":"Waiting on host to start game",
+				"Do you see your name on the screen?":"Do you see your name on the screen?"
+			},
+			"new_game":
+			{
+				"Game | KahootClone": "Game | KahootClone",
+				"Name: ":"Name: ",
+				"Score: ":"Score: "
+			}
 		},
-		"game":
+		"es":
 		{
-			"en":
-				{"pagename": "Game | LlixHoot",
-					"questionNum": "Question 1 / x",
-					"playersAnswered":"Player's answer: 0 / x",
-					"timerText": "Time left:",
-					"question": "Question",
-					"answer": "Answer",
-					"nextQuestion":"Next Question"
-				},
-			"es":
-				{"pagename": "Jugar | LlixHoot",
-					"questionNum": "Pregunta 1 / x",
-					"playersAnswered":"Respondieron: 0 / x",
-					"timerText": "Tiempo restante:",
-					"question": "Pregunta",
-					"answer": "Respuesta",
-					"nextQuestion":"Next Question"
-				},
-			"ca":
-				{"pagename": "Host | LlixHoot",
-					"questionNum": "Question 1 / x",
-					"playersAnswered":"Players Answered: 0 / x",
-					"title": "Join this Game using the Game Pin: ",
-					"cancel": "Cancel Game",
-					"start": "Start Game",
-					"nextQuestion":"Next Question"
-				}
+			"index":
+			{
+				"Join | KahootClone":"Únete | LlixHoot",
+				"Join a Game": "Unirse a una partida",
+				"Display Name": "Nombre a mostrar",
+				"Game Pin": "Pin del juego",
+				"Join": "Unirse",
+				"Click here to host a LlixHoot!": "¡Pulsa aquí para crear un LlixHoot!"
+			},
+			"quiz":
+			{
+				"Create | Kahoot": "Crear | LlixHoot",
+				"Start a Kahoot Game": "Iniciar un LlixHoot",
+				"Back": "Volver",
+				"Choose a Game Below or": "Elige un juego o ",
+				"Create your Own!": "¡Crea uno propio!"
+			},
+			"quiz_new":
+			{
+				"Creator | KahootClone":"Diseñador | LlixHoot",
+				"Kahoot Quiz Creator Studio":"Diseñador de Llixhoots",
+				"Quiz Title": "Título",
+				"Question 1: ": "Pregunta 1: ",
+				"Answer 1: ":"Respuesta 1: ",
+				"Answer 2: ":"Respuesta 2: ",
+				"Answer 3: ":"Respuesta 3: ",
+				"Answer 4: ":"Respuesta 4: ",
+				"Correct Answer (1-4) :": "Respuesta correcta (1-4) :",
+				"Add another question": "Añadir otra pregunta",
+				"Create Quiz": "Crear cuestionario",
+				"Cancel quiz and return to quiz selection": "Cancelar y volver"
+			},
+			"host":
+			{
+				"Host | Kahoot": "Host | LlixHoot",
+				"Join this Game using the Game Pin: ": "Introduce el pin para unirte: ",
+				"Cancel Game": "Cancelar",
+				"Start Game": "Jugar"
+			},
+			"game":
+			{
+				"Game | Kahoot": "Juego | LlixHoot",
+				"Question 1 / x": "Pregunta 1 / x",
+				"Players Answered: 0 / x":"Preguntas respondidas: 0 / x",
+				"Time Left:": "Tiempo restante:",
+				"Question": "Pregunta",
+				"Answer": "Respuesta",
+				"Next Question":"Siguiente pregunta"
+			},
+			"player":
+			{
+				"Lobby | KahootClone": "Lobby | LlixHoot",
+				"Waiting on host to start game":"Esperando al servidor para empezar...",
+				"Do you see your name on the screen?":"¿Ves tu nombre en la pantalla?"
+			},
+			"new_game":
+			{
+				"Game | KahootClone": "Partida | LlixHoot",
+				"Name: ":"Nombre: ",
+				"Score: ":"Puntos: "
+			}
+
 		},
+		"ca":
+		{
+			"index":
+			{
+				"Join | LlixHoot":"Join | LlixHoot",
+				"Join a Game": "Join a Game",
+				"Display Name": "Display Name",
+				"Game Pin": "Game Pin",
+				"Join": "Join",
+				"Click here to host a LlixHoot!": "Click here to host a LlixHoot!"
+			},
+			"quiz":
+			{
+				"Create | LlixHoot": "Create | LlixHoot",
+				"Start a Kahoot Game": "Start a Kahoot Game",
+				"Back": "Back",
+				"Choose a Game Below or ": "Choose a Game Below or ",
+				"Create your Own!": "Create your Own!"
+			},
+			"quiz_new":
+			{
+				"Creator | KahootClone":"Creator | KahootClone",
+				"Llixhoot Quiz Creator Studio":"Llixhoot Quiz Creator Studio",
+				"Quiz Title": "Quiz Title",
+				"Question 1: ": "Question 1: ",
+				"Answer 1: ":"Answer 1: ",
+				"Answer 2: ":"Answer 2: ",
+				"Answer 3: ":"Answer 3: ",
+				"Answer 4: ":"Answer 4: ",
+				"Correct Answer (1-4) :": "Correct Answer (1-4) :",
+				"Add another question": "Add another question",
+				"Create Quiz": "Create Quiz",
+				"Cancel quiz and return to quiz selection": "Cancel quiz and return to quiz selection"
+			},
+			"host":
+			{
+				"Host | LlixHoot": "Host | LlixHoot",
+				"Join this Game using the Game Pin: ": "Join this Game using the Game Pin: ",
+				"Cancel Game": "Cancel Game",
+				"Start Game": "Start Game"
+			},
+			"game":
+			{
+				"Game | LlixHoot": "Game | LlixHoot",
+				"Question 1 / x": "Question 1 / x",
+				"Players Answered: 0 / x":"Players Answered: 0 / x",
+				"Time Left:": "Time Left:",
+				"Question": "Question",
+				"Answer": "Answer",
+				"Next Question":"Next Question"
+			},
+			"player":
+			{
+				"Lobby | KahootClone": "Lobby | LlixHoot",
+				"Waiting on host to start game":"Waiting on host to start game",
+				"Do you see your name on the screen?":"Do you see your name on the screen?"
+			},
+			"new_game":
+			{
+				"Game | KahootClone": "Game | KahootClone",
+				"Name: ":"Name: ",
+				"Score: ":"Score: "
+			}
+		}
 	};
-	var locale=i18n[pagename].en;
 
 	if (navigator.language.startsWith("es"))
 	{
-		locale=i18n[pagename].es;
+		locale=i18n["es"][pagename];
 	} else if (navigator.language.startsWith("ca")) {
-		locale=i18n[pagename].es;
-	} 
+		locale=i18n["es"][pagename];
+	} else {
+		locale=i18n["en"][pagename];
+	}
 	return(locale)
 }
+
+function translate(element)
+{
+	return(locale[element]);
+}
+
+function translate_element(element)
+{
+	var msgid=element.innerHTML;
+	if (msgid in locale)
+	{
+		element.innerHTML=locale[msgid];
+	} 
+}
+
+function i18n(page)
+{
+	var locale=get_i18n(page);
+	to_translate=document.getElementsByTagName("TITLE");
+	var x;
+	for (x = 0; x < to_translate.length; x++) {
+  		translate_element(to_translate[x]);
+	}			 
+	to_translate=document.getElementsByTagName("LABEL");
+	for (x = 0; x < to_translate.length; x++) {
+  		translate_element(to_translate[x]);
+	}			 
+	to_translate=document.getElementsByTagName("BUTTON");
+	for (x = 0; x < to_translate.length; x++) {
+  		translate_element(to_translate[x]);
+	}			 
+	to_translate=document.getElementsByTagName("H1");
+	for (x = 0; x < to_translate.length; x++) {
+  		translate_element(to_translate[x]);
+	}			 
+	to_translate=document.getElementsByTagName("H2");
+	for (x = 0; x < to_translate.length; x++) {
+  		translate_element(to_translate[x]);
+	}			 
+	to_translate=document.getElementsByTagName("H3");
+	for (x = 0; x < to_translate.length; x++) {
+  		translate_element(to_translate[x]);
+	}			 
+	to_translate=document.getElementsByTagName("H4");
+	for (x = 0; x < to_translate.length; x++) {
+  		translate_element(to_translate[x]);
+	}			 
+	to_translate=document.getElementsByTagName("A");
+	for (x = 0; x < to_translate.length; x++) {
+  		translate_element(to_translate[x]);
+	}			 
+}
+
+var locale;
